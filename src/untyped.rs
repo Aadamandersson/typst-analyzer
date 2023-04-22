95a10b3a9b3c8f3f35d8078c60f912fdf08658dc
Adam Andersson <adam.m.andersson@gmail.com>
2023-04-22 08:04:58 +0200

    dump tree

--- src/untyped.rs
+++ src/untyped.rs
@@ -6,10 +6,31 @@ pub enum Element {
 }
 
 impl Element {
-    fn len(&self) -> usize {
+    pub(crate) fn len(&self) -> usize {
         match self {
             Element::Node(n) => n.text_len(),
             Element::Leaf(t) => t.text_len(),
+        }
+    }
+    
+    pub(crate) fn dump(&self) {
+        self.preorder(1)
+    }
+
+    fn preorder(&self, depth: u32) {
+        match self {
+            Element::Node(n) => {
+                println!("{:?}", n.kind());
+                for child in n.children() {
+                    for _ in 0..depth {
+                        print!("  ");
+                    }
+                    child.preorder(depth + 1);
+                }
+            }
+            Element::Leaf(t) => {
+                println!("{:?}", t);
+            }
         }
     }
 }
@@ -46,6 +67,12 @@ impl Node {
 pub struct Token {
     kind: SyntaxKind,
     text: String,
+}
+
+impl std::fmt::Debug for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {:?}", self.kind, self.text)
+    }
 }
 
 impl Token {
