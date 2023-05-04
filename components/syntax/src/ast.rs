8af4da7cb01c0be811d4dbe0e1dca7639b265202
Adam Andersson <adam.m.andersson@gmail.com>
2023-05-04 14:52:18 +0200

    move some stuff

--- components/syntax/src/ast.rs
+++ components/syntax/src/ast.rs
@@ -23,6 +23,166 @@ pub trait AstToken {
     fn origin(&self) -> &SyntaxToken;
 }
 
+/// A unary operator.
+#[derive(Debug, Clone, Copy)]
+pub enum UnOp {
+    /// `+`
+    Pos,
+    /// `-`
+    Neg,
+    /// `not`
+    Not,
+}
+
+impl UnOp {
+    /// Tries to construct a `UnOp` from the given `SyntaxKind`.
+    pub fn from_kind(kind: SyntaxKind) -> Option<Self> {
+        Some(match kind {
+            SyntaxKind::Plus => Self::Pos,
+            SyntaxKind::Minus => Self::Neg,
+            SyntaxKind::Not => Self::Not,
+            _ => return None,
+        })
+    }
+
+    /// Returns the precedence of this operator.
+    pub fn prec(self) -> u8 {
+        match self {
+            UnOp::Pos | UnOp::Neg => 7,
+            UnOp::Not => 4,
+        }
+    }
+}
+
+/// The associativity of a binary operator.
+#[derive(Debug, Clone, Copy)]
+pub enum Assoc {
+    /// The operator is left associative.
+    /// E.g., `x + y + z` => `(x + y) + z`
+    Left,
+    /// The operator is right associative.
+    /// E.g., `x = y = z` => `x = (y = z)`
+    Right,
+}
+
+/// A binary operator.
+#[derive(Debug, Clone, Copy)]
+pub enum BinOp {
+    /// `+`
+    Add,
+    /// `-`
+    Sub,
+    /// `*`
+    Mul,
+    /// `/`
+    Div,
+    /// `==`
+    Eq,
+    /// `!=`
+    Ne,
+    /// `<`
+    Lt,
+    /// `<=`
+    Le,
+    /// `>`
+    Gt,
+    /// `>=`
+    Ge,
+    /// `and`
+    And,
+    /// `or`
+    Or,
+    /// `in`
+    In,
+    /// `not in`
+    NotIn,
+    /// `=`
+    Assign,
+    /// `+=`
+    AddAssign,
+    /// `-=`
+    SubAssign,
+    /// `*=`
+    MulAssign,
+    /// `/=`
+    DivAssign,
+}
+
+impl BinOp {
+    /// Tries to construct a `BinOp` from the given `SyntaxKind`.
+    pub fn from_kind(kind: SyntaxKind) -> Option<Self> {
+        Some(match kind {
+            SyntaxKind::Plus => Self::Add,
+            SyntaxKind::Minus => Self::Sub,
+            SyntaxKind::Star => Self::Mul,
+            SyntaxKind::Slash => Self::Div,
+            SyntaxKind::EqEq => Self::Eq,
+            SyntaxKind::Ne => Self::Ne,
+            SyntaxKind::Lt => Self::Lt,
+            SyntaxKind::Le => Self::Le,
+            SyntaxKind::Gt => Self::Gt,
+            SyntaxKind::Ge => Self::Ge,
+            SyntaxKind::And => Self::And,
+            SyntaxKind::Or => Self::Or,
+            SyntaxKind::In => Self::In,
+            SyntaxKind::Eq => Self::Assign,
+            SyntaxKind::PlusEq => Self::AddAssign,
+            SyntaxKind::MinusEq => Self::SubAssign,
+            SyntaxKind::StarEq => Self::MulAssign,
+            SyntaxKind::SlashEq => Self::DivAssign,
+            _ => return None,
+        })
+    }
+
+    /// Returns the precedence of this operator.
+    pub fn prec(self) -> u8 {
+        match self {
+            BinOp::Mul | BinOp::Div => 6,
+            BinOp::Add | BinOp::Sub => 5,
+            BinOp::Eq
+            | BinOp::Ne
+            | BinOp::Lt
+            | BinOp::Le
+            | BinOp::Gt
+            | BinOp::Ge
+            | BinOp::In
+            | BinOp::NotIn => 4,
+            BinOp::And => 3,
+            BinOp::Or => 2,
+            BinOp::Assign
+            | BinOp::AddAssign
+            | BinOp::SubAssign
+            | BinOp::MulAssign
+            | BinOp::DivAssign => 1,
+        }
+    }
+
+    /// Returns the associativity of this binary operator.
+    pub fn assoc(self) -> Assoc {
+        match self {
+            BinOp::Add
+            | BinOp::Sub
+            | BinOp::Mul
+            | BinOp::Div
+            | BinOp::Eq
+            | BinOp::Ne
+            | BinOp::Lt
+            | BinOp::Le
+            | BinOp::Gt
+            | BinOp::Ge
+            | BinOp::And
+            | BinOp::Or
+            | BinOp::In
+            | BinOp::NotIn => Assoc::Left,
+            BinOp::Assign
+            | BinOp::AddAssign
+            | BinOp::SubAssign
+            | BinOp::MulAssign
+            | BinOp::DivAssign => Assoc::Right,
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub enum Expr {
     Literal(Literal),
@@ -128,8 +288,7 @@ impl BinaryExpr {
             .children_with_tokens()
             .filter_map(|it| it.into_token())
             .find_map(|tok| {
-                // TODO: move `BinOp` out from parser
-                if crate::parser::BinOp::from_kind(tok.kind()).is_some() {
+                if BinOp::from_kind(tok.kind()).is_some() {
                     Some(tok)
                 } else {
                     None
